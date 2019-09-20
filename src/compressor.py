12a8ea050088cea9e2836d69876c961f684ed479
Nikita Nikitin <Nikita-fin@yandex.ru>
2019-09-20 08:17:07 +0300

    Create compressor.py

---
+++ src/compressor.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+
+'''
+Methods for word vectors compression and working with compressed vectors.
+
+Методы сжатия ВП и работы с квантованными векторами.
+'''
+
+'''
+Метод записи сжатых ВП в текстовый файл.
+
+Вход:
+    Filename (str) - путь к файлу;
+    ArrayCompressedWE (np.array(np.uint8)) - массив сжатых ВП.
+Выход:
+    Нет.
+'''
+def ExportCompressedWE(Filename, ArrayCompressedWE):
+  ...
+  return 0
+  
+
+'''
+Метод импорта сжатых ВП.
+
+Вход:
+    Filename (str) - путь к файлу.
+Выход:
+    ArrayCompressedWE (np.array(np.uint8)) - массив сжатых ВП.
+'''
+def ImportCompressedWE(Filename):
+    ...
+    return ArrayCompressedWE
+
+
+'''
+Метод, записывающий координаты центроидов кластеров в текстовый файл.
+
+Вход:
+    Filename (str) - путь к файлу;
+    ArrayCentroinds (np.array(np.float32)) - 3-мерный массив [ListSubvectorSize, ListClusterSize, SubvectorSize],
+        содержащий координаты центроидов кластеров;
+    ListSubvectorSize (int) - число подвекторов, например, 75, если 300-мерный вектор разбивается на 4-мерные подвектора;
+    ListClusterSize (int) - число кластеров для каждого подвектора, например, 256, т.к. удобно хранить в uint8 / byte;
+    SubvectorSize (int) - размерность подвектора, например 4.
+Выход:
+    Нет.
+'''
+def ExportCentroids(Filename, ArrayCentroinds, ListSubvectorSize, ListClusterSize, SubvectorSize):
+    ...
+    return 0
+
+
+'''
+Метод, считывающий координаты центроидов кластеров из текстового файла.
+
+Вход:
+    Filename (str) - путь к файлу;
+    ListClusterSize (int) - число кластеров для каждого подвектора, например, 256, т.к. удобно хранить в uint8 / byte;
+    SubvectorSize (int) - размерность подвектора, например 4.
+Выход:
+    ArrayCentroids (np.array(np.float32)) - 3-мерный массив [ListSubvectorSize, ListClusterSize, SubvectorSize],
+        содержащий координаты центроидов кластеров.
+    
+'''
+def ImportCentroids(Filename, ListClusterSize, SubvectorSize):
+    ...
+    return ArrayCentroids
+
+
+'''
+Метод сжатия ВП.
+Вход:
+    ArrayWE (np.array(np.float32)) - массив векторных представлений;
+    VocabularySize (int) - словарный запас, которым мы хотим оперировать, например, 100000 словоформ;
+    EmbeddingSize (int) - размерность пространства ВП;
+    ListClusterSize (int) - число кластеров для каждого подвектора, например, 256, т.к. удобно хранить в uint8 / byte;
+    SubvectorSize (int) - размерность подвектора, например 4;
+    BatchSize - размер выборки для поиска кластеров методом MiniBatchKMeans,
+        например, 1000.
+Выход:
+    ArrayCentroids (np.array(np.float32)) - 3-мерный массив [ListSubvectorSize, ListClusterSize, SubvectorSize],
+        содержащий координаты центроидов кластеров;
+    ArrayCompressedWE (np.array(np.uint8)) - массив сжатых ВП.
+'''
+def CompressWE(ArrayWE, VocabularySize, EmbeddingSize, ListClusterSize, SubvectorSize, BatchSize):
+    ...
+    return ArrayCentroids, ArrayCompressedWE
+
+'''
+Метод сжатия ВП в ситуации, когда центроиды кластеров заранее известны.
+Вход:
+    ArrayWE (np.array(np.float32)) - массив векторных представлений;
+    VocabularySize (int) - словарный запас, которым мы хотим оперировать, 
+        например, 100000 словоформ;
+    EmbeddingSize (int) - размерность пространства ВП;
+    ListClusterSize (int) - число кластеров для каждого подвектора, например, 256, т.к. удобно хранить в uint8 / byte;
+    SubvectorSize (int) - размерность подвектора, например 4;
+    BatchSize (int) - размер выборки для поиска кластеров методом MiniBatchKMeans,
+        например, 1000;
+    ArrayCentroids (np.array(np.float32)) - 3-мерный массив [ListSubvectorSize, ListClusterSize, SubvectorSize],
+        содержащий координаты центроидов кластеров.
+Выход:
+    ArrayCompressedWE (np.array(np.uint8)) - массив сжатых ВП.
+'''
+def CompressWEwithClusters(ArrayWE, VocabularySize, EmbeddingSize, ListClusterSize, SubvectorSize, BatchSize, ArrayCentroids):
+    ...
+    return ArrayCompressedWE
+
+'''
+Метод разжатия ВП. Сжатую модель ВП всегда можно привести к состоянию,
+близкому к исходному, применив данный метод.
+Вход:
+    ArrayCompressedWE (np.array(np.uint8)) - массив сжатых ВП;
+    ArrayCentroids (np.array(np.float32)) - 3-мерный массив [ListSubvectorSize, ListClusterSize, SubvectorSize],
+        содержащий координаты центроидов кластеров;
+    EmbeddingSize (int) - размерность пространства ВП.
+Выход:
+    ArrayWE (np.array(np.float32)) - массив векторных представлений.
+'''
+def DecompressWE(ArrayCompressedWE, ArrayCentroids, EmbeddingSize):
+    ...
+    return ArrayWE
+
+'''
+Метод разжатия одного ВП. В отличие от разжатия списка ВП в DecompressWE.
+Вход:
+    CompressedWE (np.uint8) - сжатое ВП;
+    ArrayCentroids (np.array(np.float32)) - 3-мерный массив [ListSubvectorSize, ListClusterSize, SubvectorSize],
+        содержащий координаты центроидов кластеров;
+    EmbeddingSize (int) - размерность пространства ВП.
+Выход:
+    WE (np.array(np.float32)) - векторные представление.
+'''
+def DecompressSingleWE(CompressedWE, ArrayCentroids, EmbeddingSize):
+    ...
+    return WE
+
+
+'''
+Метод кластеризации сжатых ВП для быстрого поиска по областям k-мерного пространства.
+Вход:
+    ArrayWE - массив ВП для генерации кластеров верхнего уровня;
+    ArrayCompressedWE - массив сжатых ВП;
+    ListClusterSize - число кластеров (подкластеров в кластере);
+    EmbeddingSize - размерность пространства ВП;
+    GlobalArrayCentroids - массив центроидов для ArrayCompressedWE.
+Выход:
+    ArrayCentroids - массив центроидов кластеров Вороного;
+    ListBoolSubCluster - список указателей на то, имеет ли кластер подкластеры: если 0, то не имеет, а если не ноль то это глобальный индекс кластера;
+    ListClusterListPoints - список индексов точек из ArrayCompressedWE, входящих в кластер при условии, что ListBoolSubCluster[i] == 0. В противном случае имеем глобальный индекс кластера;
+    ListArrayCentroids - список массивов центороидов вложенных кластеров (подкластеров) Вороного;
+    ListListClusterListPoints - список индексов точек из ArrayCompressedWE, входящих в подкластеры.
+''' 
+def VoronoiClustering(ArrayWE, ArrayCompressedWE, ListClusterSize, EmbeddingSize, GlobalArrayCentroids):
+    ...
+    return ArrayCentroids, ListBoolSubCluster, ListClusterListPoints, ListArrayCentroids, ListListClusterListPoints 
+
+ 
