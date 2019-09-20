26b02a47d373ae2eaf8e1cb95cd656a423356d40
Nikita Nikitin <Nikita-fin@yandex.ru>
2019-09-20 22:48:54 +0300

    Update Compressor.py

--- src/Compressor.py
+++ src/Compressor.py
@@ -226,25 +226,3 @@ def DecompressWE(CompressedWE, ArrayCentroids, EmbeddingSize): #EX: DecompressSi
     WE = np.asarray(WE, np.float32).reshape(EmbeddingSize)
     
     return WE
-
-
-'''
-Метод кластеризации сжатых ВП для быстрого поиска по областям k-мерного пространства.
-Вход:
-    ArrayWE - массив ВП для генерации кластеров верхнего уровня;
-    ArrayCompressedWE - массив сжатых ВП;
-    ListClusterSize - число кластеров (подкластеров в кластере);
-    EmbeddingSize - размерность пространства ВП;
-    GlobalArrayCentroids - массив центроидов для ArrayCompressedWE.
-Выход:
-    ArrayCentroids - массив центроидов кластеров Вороного;
-    ListBoolSubCluster - список указателей на то, имеет ли кластер подкластеры: если 0, то не имеет, а если не ноль то это глобальный индекс кластера;
-    ListClusterListPoints - список индексов точек из ArrayCompressedWE, входящих в кластер при условии, что ListBoolSubCluster[i] == 0. В противном случае имеем глобальный индекс кластера;
-    ListArrayCentroids - список массивов центороидов вложенных кластеров (подкластеров) Вороного;
-    ListListClusterListPoints - список индексов точек из ArrayCompressedWE, входящих в подкластеры.
-''' 
-def VoronoiClustering(ArrayWE, ArrayCompressedWE, ListClusterSize, EmbeddingSize, GlobalArrayCentroids):
-    ...
-    return ArrayCentroids, ListBoolSubCluster, ListClusterListPoints, ListArrayCentroids, ListListClusterListPoints 
-
- 
