5fe77b5af09aaefc488dce3583af1801a4c760b3
Nikita Nikitin <Nikita-fin@yandex.ru>
2019-09-23 21:47:44 +0300

    Update Voronoi.py

--- src/Voronoi.py
+++ src/Voronoi.py
@@ -8,7 +8,7 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
 import numpy as np
 
-import Algebra
+import OpenTextbot.src.Algebra
 
 '''
 Класс для представления ассиметричных ячеек Вороного.
