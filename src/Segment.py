4af82140ba4c254e0afc9795034b72067364f30b
Nikita Nikitin <Nikita-fin@yandex.ru>
2020-02-02 11:27:13 +0300

    Update Segment.py

--- src/Segment.py
+++ src/Segment.py
@@ -1,3 +1,9 @@
 '''
 Class and methods for working with segments.
 '''
+
+class Segment(object):
+  
+  def __init__(self, ListTE, ListTokenID):
+    self.ListTE = ListTE
+    self.ListTokenID = ListTokenID
