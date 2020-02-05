e185ec7c09b05971ac9fac563d74f68dc525da11
Nikita Nikitin <Nikita-fin@yandex.ru>
2020-02-05 10:55:57 +0300

    Update Segment.py

--- src/Segment.py
+++ src/Segment.py
@@ -2,8 +2,20 @@
 Class and methods for working with segments.
 '''
 
+'''
+
+'''
+ListAttractorLength = 10
+
 class Segment(object):
   
   def __init__(self, ListTE, ListTokenID):
     self.ListTE = ListTE
     self.ListTokenID = ListTokenID
+  
+  def mean(self):
+    '''
+    1). 1 среднее значение
+    2). среднее значение+ нули, длина равно len(ListTE)
+    3). ListAttractor
+    '''
