a6dcc054880cc08614a663bf1b6e42a8b5b68e9d
Nikita Nikitin <Nikita-fin@yandex.ru>
2020-03-12 19:38:58 +0300

    Delete NLP1.py

--- Medium/NLP1.py
+++
@@ -1,7 +0,0 @@
-TestWords = ['kettle', 'fly', 'bed']
-for Word in TestWords:
-  TestWE = Algebra.Mean(bpemb_en.embed('kettle'))
-  ListWE = [ObjectEmbedding, ProcessEmbedding]
-  ListD = ['Object', 'Process']
-  TWE = Algebra.EuclidianMax(ListWE, TestWE)
-  print('\''+ Word + '\' is the ' + ListD[TWE] + '.')
