1099376b48facaf2bd9b6d3c47ef781ca9d5c96f
Nikita Nikitin <Nikita-fin@yandex.ru>
2021-01-27 23:50:20 +0300

    Update Knowledge.py

--- src/Knowledge.py
+++ src/Knowledge.py
@@ -1,2 +1,8 @@
-class Knowledge(object):
-  ...
+class KnowledgeMin(object):
+  def __init__(self, ID, Domen, ICM, FCI, KE=None):
+    self.ID = ID
+    self.Domen = Domen
+    self.ICM = ICM
+    self.FCI = FCI
+    self.KE = KE #Knowledge Embedding
+   
